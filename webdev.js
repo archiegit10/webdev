1712fb51a32ff480b810b7be3705b5f199164a21
archiegit10 <archiegit@gmail.com>
2021-10-12 10:58:20 +0100

    losing the will to live

--- webdev.js
+++ webdev.js
@@ -7,3 +7,18 @@ starsign = "cancer "
 
 console.log(firstname + surname + birthplace + starsign)
 console.log("This is a %c colour box","color: blue;font-family:helvetica; font-style: italic; background-color: red;padding: 50px");
+
+let a;
+let b = "12345";
+let c = 12344;
+let d = true;
+let e = {a:"JavaScript"};
+typeof(a)
+typeof(b)
+typeof(c)
+typeof(d)
+typeof(e)
+let totalMoney = 4000;
+let moneyPaidSoFar = 2348;
+let totalLeftToPay;
+console.log(`the total bill is £${totalMoney} with £${moneyPaidSoFar} paid so far, and £${totalLeftToPay} left to pay`)
