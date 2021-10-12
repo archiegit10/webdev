bfffd8559451498d975c6e63ae3c3bbd1fa94570
archiegit10 <archiegit@gmail.com>
2021-10-12 09:56:24 +0100

    dd

---
+++ webdev.js
@@ -0,0 +1,9 @@
+console.log("print line")
+
+firstname = "archie "
+surname = "ward "
+birthplace = "hastings "
+starsign = "cancer "
+
+console.log(firstname + surname + birthplace + starsign)
+console.log("This is a %c colour box","color: blue;font-family:helvetica; font-style: italic; background-color: red;padding: 50px");
